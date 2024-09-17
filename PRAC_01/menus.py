44ad957e3c0b8a13fa5943cc1e6352af8b40bfc8
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-09-18 09:53:32 +1000

    Wrote main code, not working as expected

--- PRAC_01/menus.py
+++ PRAC_01/menus.py
@@ -16,3 +16,19 @@ while choice != quit option
 do final thing, if needed
 """
 
+name = input("Enter your name: ")
+Menu = """ (H)ello
+(G)oodbye
+(Q)uit"""
+choice = input(">>> ").upper()
+while choice != Q:
+    if choice == "H":
+        print(f"Hello {name}")
+        choice = input(">>> ").upper()
+    elif choice == "G":
+        print(f"Goodbye {name}")
+        choice = input(">>> ").upper()
+    else:
+        print("Invalid input")
+        choice = input(">>> ").upper()
+print("Finished")
