9543a8f0e2177138d831b9d959433560d5e8560a
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-09-17 16:44:40 +1000

    Wrote code for printing stars in range of input given, commented code for better clarity

--- PRAC_01/loops.py
+++ PRAC_01/loops.py
@@ -9,3 +9,8 @@ print()
 for k in range(20,0,-1):
     print(k, end=' ')
 print()
+
+number_of_stars = int(input("Number of stars? "))
+for i in range(number_of_stars): # iterate over the range of the number held by the variable number_of_stars
+    print("*", end=" ") # print star, end=" " stays on the same line
+print() # move to next line once range has been iterated over
