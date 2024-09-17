0af86b02e561a9c71e08057a395aae5b71baeb8b
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-09-17 17:20:32 +1000

    Wrote initial code, but not working as anticipated

--- PRAC_01/shop_calculator.py
+++ PRAC_01/shop_calculator.py
@@ -13,3 +13,16 @@ Get number of items
         if items total more than $100 apply 10% discount
     display total
 """
+
+number_of_items = int(input("Enter the number of items: "))
+while number_of_items > 0:
+    for i in range(1, number_of_items + 1):
+        price = float(input("Price of item: $"))
+        total_price =+ price
+        if total_price >= 100:
+            new_total_price = total_price * 0.9
+        else:
+            new_total_price = total_price
+        print(f"Total price for {number_of_items} items is ${new_total_price:.2f}")
+print("Thankyou and please come again")
+
