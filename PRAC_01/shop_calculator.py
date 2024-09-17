903e74b208494dfb490d02dd354f482e6e04643c
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-09-17 17:04:31 +1000

    Created shop_calculator.py file and wrote psuedocode for program

---
+++ PRAC_01/shop_calculator.py
@@ -0,0 +1,15 @@
+"""
+The program allows the user to enter the number of items and the price of each different item.
+Then the program computes and displays the total price of those items.
+If the total price is over $100, then a 10% discount is applied to that total before the amount
+ is displayed on the screen.
+"""
+
+"""
+Get number of items
+    check number is greater than 0
+    display number of items
+    ask for price of each items
+        if items total more than $100 apply 10% discount
+    display total
+"""
