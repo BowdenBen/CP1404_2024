28d355f9866d1b3b6fbeb90541b7448c72668888
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-09-17 16:51:50 +1000

    Wrote code for printing stars in range of input given as consecutive, increasing lines of stars, commented code for better clarity

--- PRAC_01/loops.py
+++ PRAC_01/loops.py
@@ -14,3 +14,8 @@ number_of_stars = int(input("Number of stars? "))
 for i in range(number_of_stars): # iterate over the range of the number held by the variable number_of_stars
     print("*", end=" ") # print star, end=" " stays on the same line
 print() # move to next line once range has been iterated over
+
+number_of_stars = int(input("Number of stars? "))
+for i in range(1, number_of_stars + 1): # iterate from 1 up to and including number_of_stars
+    print("*" * i) # print star for each iteration, (notice no end=" " therefore will print each iteration on following line
+print() # move to next line once range has been iterated over
