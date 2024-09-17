03dd01b2b0389feca4848af25b5d0cdd0a5406ae
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-09-17 17:48:48 +1000

    Corrected code and commented all updates.

--- PRAC_01/shop_calculator.py
+++ PRAC_01/shop_calculator.py
@@ -19,9 +19,9 @@ while number_of_items < 0:
     print("Invalid number of items!")
     number_of_items = int(input("Enter the number of items (0 to quit): "))
 
-if number_of_items > 0:
-    total_price = 0  # Initialize total price for this transaction
+total_price = 0  # Initialize total price
 
+if number_of_items > 0:
     # Inner loop to get the price of each item
     for i in range(1, number_of_items + 1):
         price = float(input(f"Price of item {i}: $"))
@@ -35,4 +35,5 @@ if number_of_items > 0:
 print(f"Total price for {number_of_items} items is ${total_price:.2f}")
 
 print("Thank you and please come again")
+
 
