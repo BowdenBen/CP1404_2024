da42b7f899e007757101c38e543c8d81f110e8af
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-09-18 11:04:23 +1000

    Added while loop to ensure tariff selection is correct. While loop compares strings instead of integers so numbers and letters can be entered and won't break the program

--- PRAC_01/electricity_bill.py
+++ PRAC_01/electricity_bill.py
@@ -15,11 +15,17 @@ Estimated bill: $141.75
 TARIFF_11 = 0.244618
 TARIFF_31 = 0.136928
 
-tariff_select = int(input("Enter tariff, 11 or 31: ")) # Get user input selection
-if tariff_select == 11: # if, elif compared to an integer
-    tariff_rate = TARIFF_11 # if integer = 11 then put CONSTANT TARIFF_11 into variable tariff_rate
-elif tariff_select == 31:
+tariff_select = input("Enter tariff, 11 or 31: ") # Get user input selection
+while tariff_select != "11" and tariff_select != "31": #while loop for error checking tariff select
+    print("Invalid tariff selected")
+    tariff_select = input("Enter tariff, 11 or 31: ")  # Get user input selection
+
+if tariff_select == "11": # if, elif compared
+    tariff_rate = TARIFF_11 # if = 11 then put CONSTANT TARIFF_11 into variable tariff_rate
+elif tariff_select == "31":
     tariff_rate = TARIFF_31
+
+
 daily_use = int(input("Enter daily use in kWh: ")) # Get user input
 billing_days = int(input("Enter number of billing days: "))
 estimated_bill = tariff_rate * daily_use * billing_days # Generate estimated bill from user inputs
